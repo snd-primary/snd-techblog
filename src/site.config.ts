5aae93bce960b6170dbdf3cdfcf58cb2caa7160d
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-16 05:06:05 +0000

    edited site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,12 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "大殺界 -daisakkai-",
+	author: "SND",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "daisakkai-dev",
+	title: "SND-techblog",
 	// Meta property used as the default description meta property
-	description: "テックブログ。Next.js/TypeScript/JavaScript/Prisma/CSS/HTML/vim/...",
+	description:
+		"Next.js/TypeScript/JavaScript/Prisma/CSS/HTML/vim/...その他フロントエンド周辺技術について備忘録をまとめるテックブログ",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "ja-JP",
 	// Meta property, found in src/components/BaseHead.astro L:42
