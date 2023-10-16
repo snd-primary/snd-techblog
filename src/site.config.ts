8cc10f29cc20c5c9f166c57c23fec435411a1d41
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-16 03:52:32 +0000

    toLocalDateString()オプションを変更

--- src/site.config.ts
+++ src/site.config.ts
@@ -8,16 +8,16 @@ export const siteConfig: SiteConfig = {
 	// Meta property used as the default description meta property
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "ja-JP",
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "en_GB",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "ja-JP",
 		options: {
-			day: "numeric",
-			month: "short",
 			year: "numeric",
+			month: "short",
+			day: "numeric",
 		},
 	},
 	// Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
