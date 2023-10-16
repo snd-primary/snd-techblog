be2c9f01d512474cdafc1b4595ef3bb1576219ed
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2023-10-16 06:00:17 +0000

    import vercel cli

---
+++ .vercel/output/static/_astro/social-image.c86289e7.js
@@ -0,0 +1,11 @@
+const a="social-image.md",t="post",o="social-image",e=`
+## Adding your own social image card to a post
+
+This post is an example of how to add a custom social image, also known as an OG image, to a blog post.
+By adding the optional ogImage property to the frontmatter of a post, [satori](https://github.com/vercel/satori) won't generate an image for this page.
+`,n={title:"Example Social Image",description:"An example post for Astro Cactus, detailing how to add a custom social image card in the frontmatter",publishDate:new Date(16747776e5),draft:!1,tags:["example","blog","image"],ogImage:"/social-card.png"},i={type:"content",filePath:"/home/snd.webdev/prod/snd-techblog/src/content/post/social-image.md",rawData:`
+title: "Example Social Image"
+publishDate: "27 January 2023"
+description: "An example post for Astro Cactus, detailing how to add a custom social image card in the frontmatter"
+tags: ["example", "blog", "image"]
+ogImage: "/social-card.png"`};export{i as _internal,e as body,t as collection,n as data,a as id,o as slug};
--- astro.config.ts
+++ astro.config.ts
@@ -7,50 +7,52 @@ import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		remarkRehype: { footnoteLabelProperties: { className: [""] } },
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		prefetch(),
-	],
-	image: {
-		domains: ["webmention.io"],
-	},
-	vite: {
-		plugins: [rawFonts([".ttf"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://astro-cactus.chriswilliams.dev/",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""]
+      }
+    },
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true
+    }
+  },
+  integrations: [mdx({}), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), prefetch()],
+  image: {
+    domains: ["webmention.io"]
+  },
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  },
+  output: "server",
+  adapter: vercel()
 });
-
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null
+        };
+      }
+    }
+  };
 }
